d8cd3b32cee65a91923d18436a96a660ae1a5f0b
FreeTurk - Emir <teknolojigamers@gmail.com>
2022-03-08 20:37:51 +0300

    pıttık

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -7,7 +7,7 @@ export default function Entry({ title, image, article, menu }) {
     return (
         <div className={(isExp ? "w-[320px] h-[320px] cursor-pointer" : menu ? "h-infoh w-5/6 xl:w-3/4 mt-16 absolute z-30" : "h-infoh w-5/6 xl:w-[90%] mx-16 mt-16 absolute z-30") + ' flex m-16 bg-slate-200 rounded-2xl border-blue-300 border-2 drop-shadow-2xl transition-all ease-in-out duration-500'} onClick={() => {setExp(!isExp);document.getElementById("right").scrollTop;}
         }>
-            <div className='p-8 w-full'>
+            <div className='p-8 w-full overflow-hidden mb-8'>
                 
                 <div className={(isExp ? "text-xl sm:flex-row sm:gap-0 gap-4 flex-col" : "flex-col md:flex-row justify-center gap-8") + ' transition-all flex flex-row justify-between h-fit w-full items-center'}>
                     <Image src={image} alt="Logo" width={isExp ? 80 : 120} height={isExp ? 80 : 120} className="transition-all"/>
@@ -17,8 +17,8 @@ export default function Entry({ title, image, article, menu }) {
                     <p className='bg-pink-300 p-2 rounded-xl font-bold'>SAFE</p></div>
                     </div>
                 </div>
-                <div className={(isExp ? "sm:h-44 h-24 pt-8" : "h-5/6 text-scroll pt-8") + " transition-all"}>
-                    <p className={(isExp ? "text-xl" : "text-3xl overflow-y-scroll h-3/4 sm:h-5/6 md:h-full") + ' h-full text-ellipsis overflow-hidden transition-all'}>
+                <div className={(isExp ? "sm:h-44 h-24 pt-8" : " grow-1 text-scroll pt-8") + " transition-all"}>
+                    <p className={(isExp ? "text-xl" : "text-3xl overflow-y-scroll") + ' h-full text-ellipsis overflow-hidden transition-all'}>
                     {article}
                     </p>
                 </div>
