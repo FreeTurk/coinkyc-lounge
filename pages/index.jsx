255bc882c6a86f1df32f2081968b236153a6134a
FreeTurk - Emir <teknolojigamers@gmail.com>
2022-03-08 17:50:49 +0300

    s

--- pages/index.jsx
+++ pages/index.jsx
@@ -51,7 +51,7 @@ export default function Home() {
       </div>
       <div id='right' className='pl-[400px] xl:pl-[400px] m-16 overflow-y-scroll w-fit grow flex flex-wrap justify-center'>
         {
-          data.filter(x => x.title.toLowerCase().includes(query.toLowerCase())).map((elementInArray, index) => (
+          data.filter(x => x.title.toLowerCase().includes(query.toLowerCase())).map((elementInArray) => (
               <Entry title={elementInArray.title}/>
             ) 
           )
