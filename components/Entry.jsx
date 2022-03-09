033892ee5a0cd837b937293d2df6b872cc0d8ef1
FreeTurk - Emir <teknolojigamers@gmail.com>
2022-03-09 20:51:39 +0300

    lol

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -10,7 +10,7 @@ export default function Entry({ title, image, article, kyc, white, menu }) {
     return (
         <div className={(isExp ? "w-[320px] h-[320px] cursor-pointer" : menu ? "h-infoh w-5/6 xl:w-3/4 mt-16 absolute z-30" : "h-infoh w-5/6 xl:w-[90%] mx-16 mt-16 absolute z-30") + ' flex m-16 bg-slate-200 rounded-2xl border-blue-300 border-2 drop-shadow-2xl transition-all ease-in-out duration-700'} onClick={() => {setExp(!isExp);}
     }>
-            <div className='p-8 w-full overflow-hidden mb-8'>
+            <div className='p-8 w-full overflow-hidden mb-8 overflow-y-scroll'>
                 
                 <div className={(isExp ? "text-xl flex-row sm:gap-0 gap-4" : "flex-col md:flex-row justify-center gap-8") + ' transition-all flex flex-row justify-between h-fit w-full items-center'}>
                     <Image src={image} alt="Logo" width={isExp ? 80 : 120} height={isExp ? 80 : 120} className="transition-all"/>
@@ -20,8 +20,8 @@ export default function Entry({ title, image, article, kyc, white, menu }) {
                     <p className='bg-pink-300 p-2 rounded-xl font-bold'>SAFE</p></div>
                     </div>
                 </div>
-                <div className={(isExp ? "sm:h-44 h-24 pt-8" : " grow-1 text-scroll overflow-y-scroll pt-8 h-full") + " transition-all flex flex-col lg:flex-row gap-8"}>
-                    <p className={(isExp ? "text-xl hidden" : "text-3xl flex order-2") + ' h-fit text-ellipsis overflow-hidden transition-all'}>
+                <div className={(isExp ? "sm:h-44 h-24 pt-8" : " grow-1 text-scroll z-50 pt-8 h-full") + " transition-all flex flex-col lg:flex-row gap-8"}>
+                    <p className={(isExp ? "text-xl hidden" : "text-3xl flex order-2") + ' h-fit text-ellipsis transition-all'}>
                     {article}
                     </p>
                     <div className={(isExp ? '' : 'order-1 flex-col')+' flex justify-center items-center gap-4 h-fit'}>
@@ -29,7 +29,7 @@ export default function Entry({ title, image, article, kyc, white, menu }) {
                     <Image src={kyc} alt="Logo" width={300} height={225} className="transition-all"/>
                     </div>
                     <Link href={white} className={(isExp ? "block" : "hidden") + ' bg-slate-400 p-4 rounded-xl w-20 h-20 z-50'}>
-                        <div className={(isExp ? '' : 'order-1 flex-col w-64')+" bg-slate-400 mr-4 p-4 rounded-xl w-20 h-20 text-white"}>
+                        <div className={(isExp ? '' : 'order-1 flex-col w-48 lg:w-64')+" bg-slate-400 mr-4 p-4 rounded-xl w-20 h-20 text-white"}>
                         <FontAwesomeIcon icon={faArrowCircleRight}/>
                         <p className='font-black'>Docs</p>
                         </div>
